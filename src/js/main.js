92e6773b358d34b79edf22ef6f837c63b79dc719
Sergey Darchuk <darchuk.sergey@gmail.com>
2020-01-21 23:30:18 +0200

    add slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,12 @@
 $( document ).ready(function(){
-    $('body').addClass('my-app');
-    console.log('Hello !!! JavaScript is Enabled and JQuery is Working Good!)))');
+    // $('body').addClass('my-app');
+    // console.log('Hello !!! JavaScript is Enabled and JQuery is Working Good!)))');
+    $('.slider').slick({
+        nextArrow: '<img src="images/slider/white_arrow.svg" class="nextArrow">',
+        dots:true,
+        centerMode: true,
+        centerPadding: '150px 0 0'
+        // dotsClass:'dottt',
+    });   
+        
 });
