9aac65936e136e70af2d6ca31571d1abcdbbdef6
Sergey Darchuk <darchuk.sergey@gmail.com>
2020-01-22 22:19:03 +0200

    add slider and our block

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,17 @@ $( document ).ready(function(){
         nextArrow: '<img src="images/slider/white_arrow.svg" class="nextArrow">',
         dots:true,
         centerMode: true,
-        centerPadding: '150px 0 0'
-        // dotsClass:'dottt',
+        centerPadding: '150px 0 0',
+        asNavFor:'.slider-inter',
+        
+    }); 
+    $('.slider-inter').slick({
+        asNavFor:'.slider',
+        focusOnSelect: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true, 
     });   
         
 });
