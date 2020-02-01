45d4d1611d96b5e21c7c2e8fb1bffb885d2c4702
Sergey Darchuk <darchuk.sergey@gmail.com>
2020-02-01 23:57:33 +0200

    add header responcive

--- dev/js/main.js
+++ dev/js/main.js
@@ -13396,5 +13396,19 @@ $( document ).ready(function(){
         arrows: false,
         fade: true, 
     });   
+    (function($){
+        $(function() {
+            $('.menu__icon').on('click', function() {
+            $(this).closest('.menu')
+                .toggleClass('menu_state_open');
+            });
+            
+            $('.menu__links-item').on('click', function() {
+            // do something
         
+            $(this).closest('.menu')
+                .removeClass('menu_state_open');
+            });
+        });
+        })(jQuery);
 });
--- src/js/main.js
+++ src/js/main.js
@@ -17,5 +17,19 @@ $( document ).ready(function(){
         arrows: false,
         fade: true, 
     });   
+    (function($){
+        $(function() {
+            $('.menu__icon').on('click', function() {
+            $(this).closest('.menu')
+                .toggleClass('menu_state_open');
+            });
+            
+            $('.menu__links-item').on('click', function() {
+            // do something
         
+            $(this).closest('.menu')
+                .removeClass('menu_state_open');
+            });
+        });
+        })(jQuery);
 });
